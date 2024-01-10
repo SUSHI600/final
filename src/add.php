313e25a94ff89e156f162eafbb13516e1eea6cf4
SUSHI600 <shinmatsuo0719@gmail.com>
2024-01-10 14:57:21 +0900

    add src

---
+++ src/add.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>final</title>
+</head>
+<body>
+<h2>トレーニングを追加</h2>
+<a href=".php">トップに戻る</a>
+<hr>
+<form action="" method="post">
+    部位：<input type="text" name="newpart">
+    <br>
+    トレーニング名：<input type="text" name="newtraining">
+    <br>
+    <button type="submit" name="action" value="send">登録</button>
+    <br>
+</form>
+</body>
+</html>
---
+++ src/list.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>final</title>
+</head>
+<body>
+    <h2>トレーニング一覧</h2>
+    <a href="top.php">トップに戻る</a>
+</body>
+</html>
