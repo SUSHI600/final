d1c7c3274c18b11e9d4b5378d19acc03555ea5f9
SUSHI600 <shinmatsuo0719@gmail.com>
2024-01-10 14:16:41 +0900

    make file

---
+++ src/top.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>final</title>
+</head>
+<body>
+    <h2>トレーニングメニュー</h2>
+    <form action=".php" method="post">
+        <button type="submit" name="select">一覧</button>
+        <button type="submit" name="insert">登録</button>
+        <button type="submit" name="update">更新</button>
+        <button type="submit" name="delete">削除</button>
+    </form>
+</body>
+</html>
